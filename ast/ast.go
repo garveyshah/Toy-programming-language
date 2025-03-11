6f0200e2142617334a789ba664508840cbaa886b
garveyshah <garveyshah1595@gmail.com>
2025-03-11 16:47:54 +0300

    Add - InfixExpression data structures and functions

--- ast/ast.go
+++ ast/ast.go
@@ -74,6 +74,29 @@ type ExpressionStatement struct {
 	Expression Expression
 }
 
+// reperesents an infix expression
+type InfixExpression struct {
+	Token token.Token // The operator token, e.g +
+	Left Expression
+	Operator string
+	Right Expression
+}
+
+func (ie *InfixExpression) ExpressionNode() {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+
+}
+
 
 func (vs *VarStatement) statementNode()       {}
 func (vs *VarStatement) TokenLiteral() string { return vs.Token.Literal }
